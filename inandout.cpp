37f37449cb421ea720aebabae142eb079a4286eb
Johann Buere <johannbuere@gmail.com>
2025-01-02 06:37:20 +0800

    Usage of input in C++

---
+++ inandout.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+int main(){
+
+    // output = cout << (extractor operator)
+    // input = cin >> (insertion operator)
+
+    std::string name;
+    std::string fullname;
+
+    std::cout <<  "What's your name?: ";
+    std::cin >> name;
+    // this reading of inputs ends if it has hit a space or see's space so the firts name is only possible
+
+
+    std::cout <<  "Hello! I'm " << name << std::endl;
+    
+    std::cin.ignore();
+    std::cout <<  "What's your full name?: ";
+    std::getline(std::cin, fullname);
+
+    std::cout <<  "Hello! I'm " << fullname;
+    // this reading of inputs ends if it has hit a space or see's space so the firts name is only possible
+    return 0;
+}
