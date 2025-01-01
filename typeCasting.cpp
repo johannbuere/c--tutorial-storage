097bab358a2468eae70fd3522ba168795ef81bdb
Johann Buere <johannbuere@gmail.com>
2025-01-01 18:30:51 +0800

    Usage of typedef in c++

---
+++ typeCasting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    /* type conversion
+        implicit = automatic (no need of preceding values)
+        explicit = precede value with new data type
+    */
+
+   double x = 3.14;
+
+   // making it into a new data type
+   int z = (int)x;
+
+   std::cout << x;  
+
+}
