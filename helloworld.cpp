e4a4697e20ad411fd955500f9fc42b7633bf339b
Johann Buere <johannbuere@gmail.com>
2025-01-01 15:26:10 +0800

    Initial Commit

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    //lmao newbie comments
+    /*
+        This is a 
+        multi-line 
+        commnets  dwib
+    */
+    std::cout << "I like her!" << std::endl;
+    std::cout << "She's really good ;)" << std::endl;
+
+    
+    return 0;
+}
