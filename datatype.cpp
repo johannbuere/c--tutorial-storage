02353bddb1ba5ca2adc036616149b35d9f69522f
Johann Buere <johannbuere@gmail.com>
2025-01-01 17:37:28 +0800

    Introduced to c++ data types

---
+++ datatype.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main(){
+
+    // integer
+    int year = 2025;
+
+    //floats and doubles (decimals and fractions)
+    double age = 20.4;
+
+    //characters (only one)
+    char dash = '-' ;
+
+    //boolean (true or false)
+    bool yes = true;
+    bool no = false;
+
+    //strings
+    std::string name = "Johann";
+
+    std::cout << name;
+
+
+}
